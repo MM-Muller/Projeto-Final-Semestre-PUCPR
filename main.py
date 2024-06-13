a521d85f1430c16ab8b31de1ae4cdc899910a3b1
Matheus Müller <matheussmuller@icloud.com>
2024-06-13 19:37:26 -0300

    Area Login vinculada com o body

---
+++ main.py
@@ -0,0 +1,50 @@
+import tkinter as tk
+from tkinter import messagebox
+
+
+def check_login(username, password):
+    if username == "admin" and password == "1234":
+        return True
+    else:
+        return False
+
+
+def login():
+    username = entry_username.get()
+    password = entry_password.get()
+
+    if check_login(username, password):
+        messagebox.showinfo("Login bem-sucedido", "Bem-vindo, " + username + "!")
+        root.destroy()
+        open_main_application()
+    else:
+        messagebox.showerror("Erro de login", "Nome de usuário ou senha incorretos.")
+
+
+def open_main_application():
+    import body
+    body.main()
+
+
+# Criação da janela de login
+root = tk.Tk()
+root.title("Login")
+root.geometry("250x200")
+
+label_username = tk.Label(root, text="Nome de usuário:")
+label_username.pack(pady=(20, 0))
+
+entry_username = tk.Entry(root)
+entry_username.pack(pady=(0, 10))
+
+label_password = tk.Label(root, text="Senha:")
+label_password.pack(pady=(10, 0))
+
+entry_password = tk.Entry(root, show="*")
+entry_password.pack(pady=(0, 20))
+
+button_login = tk.Button(root, text="Login", command=login)
+button_login.pack(pady=(10, 20))
+
+# Inicialização da janela de login
+root.mainloop()
